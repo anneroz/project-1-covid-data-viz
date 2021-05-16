7bcfa6a26f52f8315f8c880ee8345b8f1318ffe3
anneroz <anneroz@gmail.com>
2021-05-16 13:14:50 +0000

    added Object.values to data.js line 9

--- scripts/data.js
+++ scripts/data.js
@@ -6,8 +6,7 @@ let sgpCases = [];
 async function loadSgpData() {
     let response = await axios.get("https://disease.sh/v3/covid-19/historical/sgp?lastdays=7");
     sgpDate.push(Object.keys(response.data.timeline.cases));
-    sgpCases.push(response.data.timeline.cases);
-    console.log(sgpDate, sgpCases);
+    sgpCases.push(Object.values(response.data.timeline.cases));
 }
 
 loadSgpData();
