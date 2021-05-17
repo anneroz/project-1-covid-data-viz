5818a681416d82c2dba4fbaacbc47d0d6649df36
anneroz <anneroz@gmail.com>
2021-05-17 09:02:34 +0000

    added search bar and font awesome icon

--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,15 @@
-const options = {
-    "chart": {
-        "type": "line",
-        "height": "100%"
-    },
-    "series": [{
-        "name": "cases",
-        "data": sgpCases
-    }],
-    "xaxis": {
-        "categories": sgpDate
-    },
-    // to show when no data
-    "noData": {
-        "text": "Please wait, loading data..."
-    }
+// read in historical data for Singapore
+
+let date = [];
+let deaths = [];
+let vaccines = [];
+
+async function loadData() {
+    let vaccineResponse = await axios.get("https://disease.sh/v3/covid-19/vaccine/coverage/countries/sgp?lastdays=all&fullData=true");
+    let totalsResponse = await axios.get("https://disease.sh/v3/covid-19/historical/sgp?lastdays=all");
+
+    console.log(vaccineResponse.data);
+    console.log(totalsResponse.data);
 }
 
-const chart = new ApexCharts(document.querySelector("#chart"), options);
-
-chart.render();
+loadData();
