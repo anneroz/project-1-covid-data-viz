94e4cb3b4cd9eba5f8ff7bec3343909eacc49bde
anneroz <anneroz@gmail.com>
2021-05-17 14:25:16 +0000

    added synchronized graph with hard coded data

--- scripts/script.js
+++ scripts/script.js
@@ -13,3 +13,64 @@ async function loadData() {
 }
 
 loadData();
+
+// setup global options for charts
+window.Apex = {
+    chart: {
+        height: 160,
+    },
+    dataLabels: {
+        // turn off data labels
+        enabled: false
+    }
+}
+
+// setup options for vaccine chart
+let optionsVaccine = {
+    series: [{
+        name: 'Vaccines Administered',
+        data: [4, 8, 15, 16, 23, 42]
+    }],
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+    },
+    colors: ['#008FFB'],
+    chart: {
+        id: 'vaccine-1',
+        group: 'vaccineVsDeath',
+        type: 'line',
+    },
+    yaxis: {
+        labels: {
+            minWidth: 100
+        }
+    }
+};
+
+let vaccineChart = new ApexCharts(document.querySelector("#chart-vaccines"), optionsVaccine)
+vaccineChart.render()
+
+// setup options for death chart
+let optionsDeath = {
+    series: [{
+        name: 'Total Deaths',
+        data: [25, 25, 27, 30, 31, 31]
+    }],
+    xaxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun']
+    },
+    colors: ['#546E7A'],
+    chart: {
+        id: 'death-1',
+        group: 'vaccineVsDeath',
+        type: 'line',
+    },
+    yaxis: {
+        labels: {
+            minWidth: 100
+        }
+    }
+};
+
+let deathChart = new ApexCharts(document.querySelector('#chart-deaths'), optionsDeath)
+deathChart.render()
